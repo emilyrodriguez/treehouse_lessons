f4c48693ffd649792e4ae329d8aacac73cc6a720
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2019-11-25 15:28:50 -0600

    Working with cookies and redirects

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,28 +1,47 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.render('index');
+    const name = req.cookies.username;
+    if (name) {
+      res.render('index', { name });
+    } else {
+      res.redirect('/hello');
+    }
 });
 
 app.get('/cards', (req, res) => {
-	res.render('card', { prompt: "Who is buried in Grant's tomb?" });
+    res.render('card', { prompt: "Who is buried in Grant's tomb?" });
 });
 
 app.get('/hello', (req, res) => {
-	res.render('hello');
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
 });
 
 app.post('/hello', (req, res) => {
-	res.render('hello', { name: req.body.username });
+  res.cookie('username', req.body.username);
+  res.redirect('/');
 });
 
-app.listen(port, () => {
-	console.log(`Application running on localhost:${port}`);
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
+
+app.listen(3000, () => {
+    console.log(`The application is running on localhost:${port}!`)
 });

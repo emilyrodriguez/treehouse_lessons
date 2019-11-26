ffcf698de3e70ed8cd03a524dae52612d334fbf4
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2019-11-26 13:52:26 -0600

    Adding randomizer, ability to move to next card, using cookie to remember username and hidding greeting and goodbye button if no username is known

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -3,11 +3,38 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+
+router.get('/', (req,res) => {
+	const numOfCards = cards.length;
+	const randomCardId = Math.floor(Math.random() * numOfCards);
+	res.redirect(`/cards/${randomCardId}?side=question`);
+});
+
 router.get('/:id', (req, res) => {
-    res.render('card', { 
-    	prompt: cards[req.params.id].question,
-    	hint: cards[req.params.id].hint
-    });
+	const { side } = req.query;
+	const { id } = req.params;
+
+	if (!side) {
+		res.redirect(`/cards/${randomCardId}?side=question`);
+	}
+
+	const name = req.cookies.username;
+	const text = cards[id][side];
+	const { hint } = cards[id];
+
+
+	const templateData = { id, text, name };
+
+	if (side === 'question') {
+		templateData.hint = hint;
+		templateData.sideToShow = 'answer';
+		templateData.sideToShowDisplay = 'Answer';
+	} else if (side === 'answer') {
+		templateData.sideToShow = 'question';
+		templateData.sideToShowDisplay = 'Question';
+	}
+
+    res.render('card', templateData);
 });
 
 

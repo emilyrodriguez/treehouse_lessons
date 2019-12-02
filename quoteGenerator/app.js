ed3d0a085a4583198ee5f5d2760c0c6b701b26d1
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2019-12-02 14:25:55 -0600

    Using try/catch with async/await and added POST route for quote creation

--- quoteGenerator/app.js
+++ quoteGenerator/app.js
@@ -1,18 +1,46 @@
 const express = require('express');
 const app = express();
+
 const records = require('./records');
 
-//Send a GET request to /quotes to READ a list of quotes
+app.use(express.json());
+
+// Send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', async (req, res) => {
-	const quotes = await records.getQuotes();
-	res.json(quotes);
+	try {
+	    const quotes = await records.getQuotes();
+	    res.json(quotes);
+	} catch(err) {
+		res.json({ message: err.message });
+	}
 });
-//Send a GET request to /quotes/:id to READ a specific quote
+
+// Send a GET request to /quotes/:id to READ(view) a quote
 app.get('/quotes/:id', async (req, res) => {
-	const quote = await records.getQuote(req.params.id);
-	res.json(quote);
+	try {
+	    const quote = await records.getQuote(req.params.id);
+	    res.json(quote);
+	} catch(err) {
+		res.json({ message: err.message });
+	}
 });
+
+//Send a POST request to /quotes to  CREATE a new quote 
+app.post('/quotes', async (req,res) => {
+	try {
+	    const quote = await records.createQuote({
+	        quote: req.body.quote,
+	        author: req.body.author
+	    });
+	    res.json(quote);
+	} catch(err) {
+		res.json({ message: err.message });
+	}
+});
+
+// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+// Send a DELETE request to /quotes/:id DELETE a quote 
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
 

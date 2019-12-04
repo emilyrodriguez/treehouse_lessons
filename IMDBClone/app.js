519e1fef3d96846b4e4ea42f7f4816643ca93c4e
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2019-12-04 11:38:29 -0600

    Creating Sequelize movie model

--- IMDBClone/app.js
+++ IMDBClone/app.js
@@ -5,12 +5,24 @@ const sequelize = new Sequelize({
   storage: 'movies.db'
 });
 
-// async IIFE
+// Movie model
+class Movie extends Sequelize.Model {}
+Movie.init({
+  title: Sequelize.STRING,
+}, { sequelize });
+
 (async () => {
+  // Sync all tables
+  await sequelize.sync({ force: true });
+
   try {
-    await sequelize.authenticate();
-    console.log('Connection to the database successful!');
+
+  	const movie = await Movie.create({
+  		 title: 'Toy Story',
+  	});
+  	console.log(movie.toJSON());
+
   } catch (error) {
-  	console.error('Error connecting to the database: ', error);
+    console.error('Error connecting to the database: ', error);
   }
 })();

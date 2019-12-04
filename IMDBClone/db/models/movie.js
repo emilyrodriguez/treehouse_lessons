6fd96ebca33231487c9e4df13f74c7bb8390062b
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2019-12-04 12:13:25 -0600

    Adding data validation

--- IMDBClone/db/models/movie.js
+++ IMDBClone/db/models/movie.js
@@ -11,19 +11,46 @@ module.exports = (sequelize) => {
     title: {
     	type: Sequelize.STRING,
     	allowNull: false,
+    	validate: { 
+    		notNull: {
+	          msg: 'Title cannot be blank.',
+	        },
+    		notEmpty: {
+    			msg: "Title cannot be blank.";
+    		}
+    	},
     },
     runtime: { 
     	type: Sequelize.INTEGER,
     	allowNull: false,
+    	validate: { 
+    		notNull: {
+	          msg: 'Please provide a value for "runtime"',
+	        },
+	        min: {
+	        	args: 1.
+	        	msg: "Runtime must be greater than 0 minutes.",
+	        },
+    	},
     },
     releaseDate: { 
     	type: Sequelize.DATEONLY,
     	allowNull: false,
+    	validate: { 
+    		notNull: {
+	          msg: 'Please provide a value for "releaseDate"',
+	        },
+	        isAfter: {
+	        	args: '1895-12-27',
+          		msg: 'Release date must be after "1895-12-28"',
+	        },
+    	},
     },
     isAvailableOnVHS: { 
     	type: Sequelize.BOOLEAN,
     	allowNull: false,
     	defaultValue: false,
+    	validate: { },
     },
   }, { sequelize });
 

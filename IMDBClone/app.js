8191315513ac0b581ae7602d09b6d45e5526f43a
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2019-12-04 11:52:38 -0600

    Creating new file structure for ease of maintainence

--- IMDBClone/app.js
+++ IMDBClone/app.js
@@ -1,33 +1,19 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'movies.db',
-  // logging: false
-});
-
-// Movie model
-class Movie extends Sequelize.Model {}
-Movie.init({
-  title: Sequelize.STRING,
-}, { sequelize });
+const db = require('./db');
+const { Movie } = db.models;
 
 (async () => {
-  // Sync all tables
-  await sequelize.sync({ force: true });
+  await db.sequelize.sync({ force: true });
 
   try {
+    const movie = await Movie.create({
+      title: 'Toy Story'
+    });
+    console.log(movie.toJSON());
 
-  	const movieInstances = await Promise.all([
-      Movie.create({
-        title: 'Toy Story'
-      }),
-      Movie.create({
-        title: 'The Incredibles'
-      }),
-    ]);
-    const moviesJSON = movieInstances.map(movie => movie.toJSON());
-    console.log(moviesJSON);
+    const movie2 = await Movie.create({
+      title: 'The Incredibles'
+    });
+    console.log(movie2.toJSON());
 
   } catch (error) {
     console.error('Error connecting to the database: ', error);
